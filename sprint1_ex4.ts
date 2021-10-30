9d22d4c54ede54900b07dcaab5f7611432373a8d
Tiago Rodrigues <tiag07desa@gmail.com>
2021-10-30 17:45:31 -0300

    sprint1_ex4 em andamento

--- sprint1_ex4.ts
+++ sprint1_ex4.ts
@@ -1,3 +1,28 @@
-console.log('aaaa')
-console.log('aaaa')
-console.log('aaaa')
+let userList : Array<Object> = 
+[
+    {id : 1, name: "Ada Lovelace", bio : "Ada Lovelace, foi uma matemática e escritora inglesa reconhecida por ter escrito o primeiro algoritmo para ser processado por uma máquina"},
+    {id : 2, name: "Alan Turing", bio : "Alan Turing foi um matemático, cientista da computação, lógico, criptoanalista, filósofo e biólogo teórico britânico, ele é amplamente considerado o pai da ciência da computação teórica e da inteligência artificia"},
+    {id : 3, name: "Nikola Tesla", bio : "Nikola Tesla foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada."},
+    {id : 4, name: "Nicolau Copérnico", bio: "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar."}
+];
+
+function tabela_Start() {
+    
+}
+
+function formulario_Start() {
+    
+}
+
+function openForm(){
+window.open('sprint1_ex4_formulario.html');
+}
+
+function btnAddItemOnTable(){
+    let inputNewName : string = document.querySelector('input#inputNewName').value;
+    let inputNewBio : string = document.querySelector('input#inputNewBio').value;
+}
+
+function refreshItemsId(){
+
+}
